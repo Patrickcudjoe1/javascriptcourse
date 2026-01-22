5a9bc437e510715b07d275ca7355c862634cf77b
paragon technolgy <patrickcudjoeh@gmail.com>
2026-01-22 21:31:47 +0000

    Add JavaScript assignments

---
+++ assignment1.js
@@ -0,0 +1,31 @@
+// Print the message "Hello programming..." to the console
+console.log("Hello programming...");
+
+// Declare a variable x and assign it the value 5
+let x = 5;
+console.log(x);
+
+// Declare a variable y and assign it the value "five"
+let y = "five";
+console.log(y);
+
+// Assign the value of y to x
+x = y;
+console.log(x);
+
+// Declare a variable called name without assigning any value
+let name;
+console.log(typeof name);
+
+// Declare a variable food and assign it the value "rice"
+let food = "rice";
+console.log(food);
+
+// Using var to declare a variable student and assign it "Ama"
+var student = "Ama";
+// Using var to Redeclare the same variable and assign it "Kofi"
+var student = "Kofi";
+console.log(student);
+// var allows redeclaration because it is function scoped.
+
+
